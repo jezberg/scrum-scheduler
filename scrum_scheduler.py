f826d37cfdb3c781e6081a97351384ee35cc0ef5
Jeremias B <jeremiasberg@gmail.com>
2024-10-24 20:51:28 +0300

    neater printing

--- scrum_scheduler.py
+++ scrum_scheduler.py
@@ -24,12 +24,14 @@ class ScrumData():
     person_in_group_meeting_slot : defaultdict[dict] # (person, group, slot) -> int, indicator variables for person participating in group meeting in slot 
     highest_var : int
 
-def print_non_sat_info(data: ScrumData) -> None:
-    print("Num slots ", data.num_slots)
-    print("People")
-    print(data.people.keys())
-    print("Groups (and members)")
-    print(groups)
+def print_data_properties(data: ScrumData) -> None:
+    print("Scrum data to compute on")
+    print("Num meeting slots: ", data.num_slots)
+    print("People: ", list(data.people.keys()))
+    print("Groups and their members")
+    for group_name, members in groups.items():
+         print("\t", group_name, ": ", members)
+    print()
 
 
 ## Expects the grous as a dictionary mapping a group name to the members of it. Will break if group names are not unique, but does not check 
@@ -41,7 +43,6 @@ def create_scrum_data(_num_slots : int, _groups : defaultdict) -> ScrumData:
             dict_people[person].add(group_name)
 
     ### then we create a mapping to the groups they belong to 
-
     _highest_var = 0
     ## add names for the indicators of group meetings these variables are meant to indicate that a gorup should keep its meeting in slot
     _group_meeting_slot_vars = defaultdict(None)
@@ -68,7 +69,7 @@ def create_wcnf(data : ScrumData) -> WCNF:
     cnf = WCNF()
     top_var = data.highest_var
     
-    print_debug("Exactly one meeting slot per group")
+    print_debug("every group has a meeting in exactly one slot")
     for group_name in data.groups:
         slot_vars = [data.group_meeting_slot_vars[(group_name, x)] for x in range(1, data.num_slots +1) ]
         print_debug("group_name ", group_name, " range ", range(1,data.num_slots+1), " slot_vars ", slot_vars)
@@ -81,7 +82,7 @@ def create_wcnf(data : ScrumData) -> WCNF:
         cnf.extend(enc.clauses)
         top_var = max(top_var, cnf.nv)
             
-    print_debug("every person has atmost one meeting in a slot" )
+    print_debug("every person has at most one meeting in a slot" )
     for person, groups in data.people.items():
         for slot in range(1, data.num_slots +1):
             slot_vars = [data.person_in_group_meeting_slot[(person, g, slot)] for g in groups ]
@@ -164,16 +165,13 @@ def interpret_model(model : List[int], data : ScrumData, missed_meetings : int) 
 def schedule_scrum_meetings(data : ScrumData) -> None:
     cnf = create_wcnf(data)
     with RC2(cnf) as rc2:
-        m = rc2.compute() 
-       # print('model {0} has cost {1}'.format(m, rc2.cost))
-        interpret_model(m, data, rc2.cost)
-       # print('model {0} has cost {1}'.format(m, rc2.cost))
+        model = rc2.compute() 
+        interpret_model(model, data, rc2.cost)
     return 
 
 
 if (__name__ == "__main__"):
     num_slots = 2
-    # a group is represented by a string "name", and a list (its members)
     groups = defaultdict(None)
     groups = {
     'g1' : {'A', 'B', 'C'},
@@ -182,6 +180,6 @@ if (__name__ == "__main__"):
     'g4' : {'D', 'E', 'F'}
     }
     scrum_data = create_scrum_data(num_slots, groups)
-    print_non_sat_info(scrum_data)
+    print_data_properties(scrum_data)
     print_debug(scrum_data)
     schedule_scrum_meetings(scrum_data)
